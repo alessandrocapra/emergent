9eea17226a9ca812110174363e2c9d2ae906d7fb
Thomas <hello@thomaslangerak.nl>
2017-04-07 17:43:33 +0300

    multiple shifts'

---
+++ Current Stuff/arduino/ShiftTest_MultipleShift/ShiftTest_MultipleShift.ino
@@ -0,0 +1,65 @@
+
+// ShiftPWM uses timer1 by default. To use a different timer, before '#include <ShiftPWM.h>', add
+// #define SHIFTPWM_USE_TIMER2  // for Arduino Uno and earlier (Atmega328)
+// #define SHIFTPWM_USE_TIMER3  // for Arduino Micro/Leonardo (Atmega32u4)
+
+// Clock and data pins are pins from the hardware SPI, you cannot choose them yourself if you use the hardware SPI.
+// Data pin is MOSI (Uno and earlier: 11, Leonardo: ICSP 4, Mega: 51, Teensy 2.0: 2, Teensy 2.0++: 22) 
+// Clock pin is SCK (Uno and earlier: 13, Leonardo: ICSP 3, Mega: 52, Teensy 2.0: 1, Teensy 2.0++: 21)
+
+// You can choose the latch pin yourself.
+const int ShiftPWM_latchPin=8;
+
+// ** uncomment this part to NOT use the SPI port and change the pin numbers. This is 2.5x slower **
+// #define SHIFTPWM_NOSPI
+//const int ShiftPWM_dataPin = 11;
+// const int ShiftPWM_clockPin = 13  ;
+
+
+// If your LED's turn on if the pin is low, set this to true, otherwise set it to false.
+const bool ShiftPWM_invertOutputs = false; 
+
+// You can enable the option below to shift the PWM phase of each shift register by 8 compared to the previous.
+// This will slightly increase the interrupt load, but will prevent all PWM signals from becoming high at the same time.
+// This will be a bit easier on your power supply, because the current peaks are distributed.
+const bool ShiftPWM_balanceLoad = false;
+
+#include <ShiftPWM.h>   // include ShiftPWM.h after setting the pins!
+
+// Here you set the number of brightness levels, the update frequency and the number of shift registers.
+// These values affect the load of ShiftPWM.
+// Choose them wisely and use the PrintInterruptLoad() function to verify your load.
+// There is a calculator on my website to estimate the load.
+
+unsigned char maxPower = 255;
+unsigned char pwmFrequency = 75;
+int numRegisters = 4;
+int numberOfOutputs=numRegisters*8;
+
+void setup(){
+  Serial.begin(9600);
+
+  // Sets the number of 8-bit registers that are used.
+  ShiftPWM.SetAmountOfRegisters(numRegisters);
+  ShiftPWM.Start(pwmFrequency,maxPower);
+}
+
+int i=0;
+
+void loop()
+{    
+  // Turn all LED's off.
+  ShiftPWM.SetAll(0);
+  // Fade in and fade out all outputs one by one fast. Usefull for testing your hardware. Use OneByOneSlow when this is going to fast.
+  Serial.println(i);
+  ShiftPWM.SetOne(i,255); 
+  i++;
+  if (i==numberOfOutputs){
+    i=0;
+  }
+  delay(500);
+}
+
+
+
+
